a7f2475ca801fe213997370bf2d95c42b83a5cbc
ArTechDev <130270173+ArTechDev@users.noreply.github.com>
2023-11-12 14:50:02 +0800

    018_sidebar

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const GlobalSearch = () => {
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
-      <div className="background-light800_darkgradient gapo-1 relative flex min-h-[56px] grow items-center rounded-xl px-4">
+      <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
         <Image
           src="/assets/icons/search.svg"
           alt="search"
@@ -16,7 +16,7 @@ const GlobalSearch = () => {
 
         <Input
           type="text"
-          placeholder="Search gloabally"
+          placeholder="Search Globally"
           value=""
           className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
--- constant/index.ts
+++ constant/index.ts
@@ -14,7 +14,7 @@ export const sidebarLinks:SidebarLink[] = [
  },
  {
     imgURL:"/assets/icons/users.svg",
-    route:"/coummunity",
+    route:"/community",
     label:"Community",
  },
  {
